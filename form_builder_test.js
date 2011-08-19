b1a9b40bcc5f6f0f3fcc231f8a15b57921d41427
Nicolas <nicolas.papon@webflows.fr>
2011-08-19 11:28:47 +0200

    added a failing test for form_for

--- form_builder_test.js
+++ form_builder_test.js
@@ -7,16 +7,19 @@ steal.plugins('funcunit').then(function(){
     });
 
     test("fields helpers", function(){
-      equals(S("form input:eq(0)").val(), "my title", "text_field value is not properly defined");
-      equals(S("form input:eq(0)").attr('name'), 'blog_post[title]', "input names are properly set")
-      equals(S("form .comments input:first").attr('name'), 'blog_post[comments][id]', "basename is incremented as expected")
-      equals(S("form .comments input:last").attr('name'), 'blog_post[comments][user_attributes][name]', "fields_for allow custom basename")
+      equals(S("#content input:eq(0)").val(), "my title", "text_field value is not properly defined");
+      equals(S("#content input:eq(0)").attr('name'), 'blog_post[title]', "input names are properly set")
+      equals(S("#content .comments input:first").attr('name'), 'blog_post[comments][id]', "basename is incremented as expected")
+      equals(S("#content .comments input:last").attr('name'), 'blog_post[comments][user_attributes][name]', "fields_for allow custom basename")
     });
 
     test('wrapper templates', function(){
-      ok(S("form .field").exists(), "default builder wrapper is output")
-      ok(S('form .field.checkbox').exists(), "alternate test wrapper is output")
+      ok(S("#content .field").exists(), "default builder wrapper is output")
+      ok(S('#content .field.checkbox').exists(), "alternate test wrapper is output")
     })
 
+    test('form_for helper generate a form tag', function(){
+      S("#content form").exists()
+    })
 
 })
